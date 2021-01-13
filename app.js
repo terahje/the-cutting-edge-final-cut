dfe9dc8d15e2656f562c537a97b8e1fd3539d8a0
aksmith5239 <aklobby@gmail.com>
2021-01-12 17:57:53 -0700

    server setup

--- app.js
+++ app.js
@@ -1,22 +1,46 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
-const PORT = 3000;
+const {graphqlHTTP} = require('express-graphql');
+const { buildSchema } = require('graphql');
+const PORT = 3001;
 
 const app = express();
 
-app.use(bodyParser.json);
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+    type RootQuery{
+        appt: [String!]!
+    }
+
+    type RootMutation {
+        createAppt(name: String): String
+    }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        appt: () => {
+            //appt resolver
+            return ['Hair Cut', 'Permanent']
+        },
+        createApp: (args) => {
+            //create appt resolver
+            const apptName = args.name;
+            return apptName;
+        }
+        
+    },
+    graphiql: true
+}));
 
 app.get('/', (req, res, next) => {
     res.send('Hello World');
 })
 
-// app.listen(3001);
 
 app.listen(PORT, () => {
     console.log(`🌎 API server running on port ${PORT}!`);
 });
-
-
 
 
