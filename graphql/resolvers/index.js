783c99d0abf5add7e3e26fb13089a4ef3ff50313
aksmith5239 <aklobby@gmail.com>
2021-01-15 08:01:20 -0700

    refactoring to modify booking function

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,8 +3,9 @@ const Appt = require('../../models/appt');
 const User = require('../../models/user');
 const Booking = require('../../models/booking');
 const { dateToString } = require('../../helpers/date');
+
 // function to hold the mapped appointment
-const newAppt = appointment => {
+const modifyAppt = appointment => {
     return {
         ...appointment._doc, 
         _id: appointment.id,
@@ -12,25 +13,35 @@ const newAppt = appointment => {
         creator: user.bind(this, appointment._doc.creator)
     };
 };
-
-
-const appt = async apptIds => {   
-   try {
-    const appt =  await Appt.find({ _id: { $in: apptIds } });
-        return appt.map(appointment => {
-            return newAppt(appointment);
-        });
-      
-    } catch (err) {
-        throw err;
+//function to hold the mapped booking
+const modifyBooking = booking => {
+    return {
+        ...booking._doc,
+        _id: booking.id,
+        user: user.bind(this, booking._doc.user),
+        appointment: singleAppt.bind(this, booking._doc.appointment),
+        createdAt: dateToString(booking._doc.createdAt),
+        updatedAt: dateToString(booking._doc.updatedAt)
     }
-};  
+};
+
+const appt = async apptId => {
+    try {
+      const appt = await Event.find({ _id: { $in: apptId } });
+      appt.map(appointment => {
+        return modifyAppt(appointment);
+      });
+      return appt;
+    } catch (err) {
+      throw err;
+    }
+  };
  
 const singleAppt = async apptId => {
     try{
         //get the appointment
         const appointment = await Appt.findById(apptId);
-        return newAppt(appointment);
+        return modifyAppt(appointment);
     }
     catch (err) {
         throw err;
@@ -38,23 +49,23 @@ const singleAppt = async apptId => {
 }
 const user = async userId => {
     try {
-    const user = await User.findById(userId);
-        return {
-            ...user._doc,
-             id: user.id, 
-             createdAppts: appt.bind(this, user._doc.createdAppts) 
-        };
+      const user = await User.findById(userId);
+      return {
+        ...user._doc,
+        _id: user.id,
+        createdAppt: appt.bind(this, user._doc.createdAppt)
+      };
     } catch (err) {
-        throw err;
+      throw err;
     }
-};
+  };
  module.exports = 
  {
     appt: async () => {
         try{
         const appt = await Appt.find()
             return appt.map(appointment => {
-                return convertAppt(appointment);
+                return modifyAppt(appointment);
             });
         }catch (err) {
             throw err;
@@ -64,16 +75,10 @@ const user = async userId => {
         try{
            //get all of the bookings from db
            const bookings = await Booking.find();
-           return bookings.map(booking=> {
-               return {
-                   ...booking._doc,
-                   _id: booking.id,
-                   user: user.bind(this, booking._doc.user),
-                   appointment: singleAppt.bind(this, booking._doc.appointment),
-                   createdAt: dateToString(booking._doc.createdAt),
-                   updatedAt: dateToString(booking._doc.updatedAt)
-               };
+           return bookings.map(booking => {
+               return modifyBooking(booking);  
            });
+          
         }
         catch (err) {
             throw err;
@@ -88,7 +93,7 @@ const user = async userId => {
            price: +args.apptInput.price,
            date: new Date(args.apptInput.date),
            time: args.apptInput.time,
-           creator: '5fff85e6a2c1af1ea3273646'
+           creator: '6001a9f6850d32286e2cdac3'
     });
     let createdAppts;
     try {
@@ -99,7 +104,7 @@ const user = async userId => {
         date: dateToString(appointment._doc.date),
         creator: user.bind(this, result._doc.creator)
       };
-      const creator = await User.findById('5fff85e6a2c1af1ea3273646');
+      const creator = await User.findById('6001a9f6850d32286e2cdac3');
 
       if (!creator) {
         throw new Error('User not found.');
@@ -140,18 +145,12 @@ const user = async userId => {
             appointment: retrievedAppt
         });
         const result = await booking.save();
-        return {...result._doc,
-           _id: result.id, 
-        user: user.bind(this, booking._doc.user),
-        appointment: singleAppt.bind(this, booking._doc.appointment),
-        createdAt: dateToString(result._doc.createdAt),
-        updatedAt: dateToString(result._doc.updatedAt)
-        };
+        return modifyBooking(result);
     },
     cancelAppt: async args => {
         try{
            const booking = await Booking.findById(args.bookingId).populate('appointment'); 
-           const appointment = convertAppt(booking.appointment);
+           const appointment = modifyAppt(booking.appointment);
             
         await Booking.deleteOne({_id: args.bookingId});
          return appointment;
