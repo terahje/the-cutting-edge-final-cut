0971f1d1874894aa7db4da0f048fdf5baba819db
Jennifer Schlierman <jd9913@gmail.com>
2021-01-15 14:02:34 -0700

    adding routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,38 @@
 import React from "react";
+import {
+	BrowserRouter as Router,
+	Route,
+	Redirect,
+	Switch,
+} from "react-router-dom";
+import Auth from "./Pages/Auth";
+import Booking from "./Pages/Booking";
+import Appt from "./Pages/Appt";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
-import HomePage from "./components/Pages/HomePage";
+import HomePage from "./Pages/HomePage";
 
 function App() {
 	return (
-		<div>
-			<Header />
-			<main className='py-3'>
-				<Container>
-					<HomePage />
-				</Container>
-			</main>
-			<Footer />
-		</div>
+		<Router>
+			<React.Fragment>
+				<Header />
+				<main className='py-3'>
+					<Switch>
+						<Redirect from='/' to='/login' exact />
+						<Route path='/' component={HomePage} exact />
+						<Route path='/login' component={Auth} />
+						<Route path='/appt' component={Appt} />
+						<Route path='/booking' component={Booking} />
+					</Switch>
+					<div>
+						<Container>This is the home page</Container>
+					</div>
+				</main>
+				<Footer />
+			</React.Fragment>
+		</Router>
 	);
 }
 
---
+++ client/src/Pages/Auth.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Auth = () => {
+	return (
+		<div>
+			<h1>This is the Auth Page</h1>
+		</div>
+	);
+};
+
+export default Auth;
