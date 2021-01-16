4d7c8ea4a674cd1104f147a14cda2ed92de6faf3
Jennifer Schlierman <jd9913@gmail.com>
2021-01-16 15:15:46 -0700

    added sign-in, sign-up form

--- client/src/Pages/Auth.js
+++ client/src/Pages/Auth.js
@@ -1,10 +1,31 @@
 import React from "react";
+import { Form, Button } from "react-bootstrap";
 
 const Auth = () => {
 	return (
-		<form>
-			<h1>This is the Login Page</h1>
-		</form>
+		<Form>
+			<Form.Group controlId='formBasicEmail'>
+				<Form.Label>Email address</Form.Label>
+				<Form.Control type='email' placeholder='Enter email' />
+				<Form.Text className='text-muted'>
+					We'll never share your email with anyone else.
+				</Form.Text>
+			</Form.Group>
+			<Form.Group controlId='formBasicPassword'>
+				<Form.Label>Password</Form.Label>
+				<Form.Control type='password' placeholder='Password' />
+			</Form.Group>
+			<Form.Group controlId='formBasicCheckbox'>
+				<Form.Check type='checkbox' label='Check me out' />
+			</Form.Group>
+			<Button variant='primary' type='button'>
+				Sign-up
+			</Button>
+			{"  "}
+			<Button variant='primary' type='submit'>
+				Login
+			</Button>
+		</Form>
 	);
 };
 
