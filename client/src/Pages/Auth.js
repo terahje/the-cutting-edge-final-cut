d86bde830755ec41ae621ff687b57d37f75f92cf
Jennifer Schlierman <jd9913@gmail.com>
2021-01-16 17:35:32 -0700

    adding headers and connection for login page

--- client/src/Pages/Auth.js
+++ client/src/Pages/Auth.js
@@ -1,32 +1,78 @@
-import React from "react";
+import React, { Component } from "react";
 import { Form, Button } from "react-bootstrap";
 
-const Auth = () => {
-	return (
-		<Form>
-			<Form.Group controlId='formBasicEmail'>
-				<Form.Label>Email address</Form.Label>
-				<Form.Control type='email' placeholder='Enter email' />
-				<Form.Text className='text-muted'>
-					We'll never share your email with anyone else.
-				</Form.Text>
-			</Form.Group>
-			<Form.Group controlId='formBasicPassword'>
-				<Form.Label>Password</Form.Label>
-				<Form.Control type='password' placeholder='Password' />
-			</Form.Group>
-			<Form.Group controlId='formBasicCheckbox'>
-				<Form.Check type='checkbox' label='Check me out' />
-			</Form.Group>
-			<Button variant='primary' type='button'>
-				Sign-up
-			</Button>
-			{"  "}
-			<Button variant='primary' type='submit'>
-				Login
-			</Button>
-		</Form>
-	);
-};
+class Auth extends Component {
+	constructor(props) {
+		super(props);
+		this.emailEl = React.createRef();
+		this.passwordEl = React.createRef();
+	}
+
+	submitHandler = (event) => {
+		event.preventDefault();
+
+		const email = this.emailEl.current.value;
+		const password = this.passwordEl.current.value;
+
+		if (email.trim().length === 0 || password.trim().length === 0) {
+			return;
+		}
+
+		const requestBody = {
+			query: `
+			mutation{
+				createUser(userInput: {email: "${email}", password: "${password}"}){
+					_id
+					email
+				}
+			}
+
+			`,
+		};
+
+		fetch("http://localhost:8000/graphql", {
+			method: "POST",
+			body: JSON.stringify(requestBody),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+	};
+	render() {
+		return (
+			<Form onSubmit={this.submitHandler}>
+				<Form.Group controlId='formBasicEmail'>
+					<Form.Label>Email address</Form.Label>
+					<Form.Control
+						type='email'
+						placeholder='Enter email'
+						ref={this.emailEl}
+					/>
+					<Form.Text className='text-muted'>
+						We'll never share your email with anyone else.
+					</Form.Text>
+				</Form.Group>
+				<Form.Group controlId='formBasicPassword'>
+					<Form.Label>Password</Form.Label>
+					<Form.Control
+						type='password'
+						placeholder='Password'
+						ref={this.passwordEl}
+					/>
+				</Form.Group>
+				<Form.Group controlId='formBasicCheckbox'>
+					<Form.Check type='checkbox' label='Check me out' />
+				</Form.Group>
+				<Button variant='primary' type='button'>
+					Sign-up
+				</Button>
+				{"  "}
+				<Button variant='primary' type='submit'>
+					Login
+				</Button>
+			</Form>
+		);
+	}
+}
 
 export default Auth;
--- server/app.js
+++ server/app.js
@@ -7,12 +7,23 @@ const graphQlSchema = require("./graphql/schema/index");
 const graphQlResolvers = require("./graphql/resolvers/index");
 
 const path = require("path");
+const { allowedNodeEnvironmentFlags } = require("process");
 
 const app = express();
 
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
+
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-methods", "POST, GET, OPTIONS");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+	if (req.method === "OPTIONS") {
+		return res.sendStatus(200);
+	}
+	next();
+});
 
 app.use(
 	"/graphql",
