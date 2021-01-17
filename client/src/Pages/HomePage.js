4609f6853547db51bb59d8e885186750f43d7a35
terahje <terahjequinn@gmail.com>
2021-01-16 21:40:07 -0700

    added frontend from jd9913

---
+++ client/src/Pages/HomePage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import services from "../services";
+import Service from "../components/Service";
+
+const HomePage = () => {
+	return (
+		<div>
+			<h1>Services</h1>
+			<Row>
+				{services.map((service) => (
+					<Col sm={12} md={6} lg={4}>
+						<Service service={service} />
+					</Col>
+				))}
+			</Row>
+		</div>
+	);
+};
+
+export default HomePage;
