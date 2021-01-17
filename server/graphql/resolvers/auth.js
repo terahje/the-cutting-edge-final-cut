45a5dad2b04078972b810ecbca7a0e4c5154c9d2
Jennifer Schlierman <jd9913@gmail.com>
2021-01-16 21:30:20 -0700

    adding login

--- server/graphql/resolvers/auth.js
+++ server/graphql/resolvers/auth.js
@@ -1,25 +1,41 @@
-const bcrypt = require('bcrypt');
-const User = require('../../models/user');
+const bcrypt = require("bcrypt");
+const User = require("../../models/user");
+const jwt = require("jsonwebtoken");
 
- module.exports = 
- {
-    createUser: async (args) => {
-        //find if the user exists
-        try {
-            const foundUser = await User.findOne({email: args.userInput.email})
-            if(foundUser) {
-                throw new Error('This user has already been created.');
-            }
-            const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
+const secret = "shhsupersecretkeyprojectbstudios";
 
-            const user = new User({
-                email: args.userInput.email,
-                password: hashedPassword
-            });
-            const result = await user.save();
-            return { ...result._doc, password: null, _id: result.id };
-        } catch (err) {
-            throw err;
-        }
-    }
+module.exports = {
+	createUser: async (args) => {
+		//find if the user exists
+		try {
+			const foundUser = await User.findOne({ email: args.userInput.email });
+			if (foundUser) {
+				throw new Error("This user has already been created.");
+			}
+			const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
+
+			const user = new User({
+				email: args.userInput.email,
+				password: hashedPassword,
+			});
+			const result = await user.save();
+			return { ...result._doc, password: null, _id: result.id };
+		} catch (err) {
+			throw err;
+		}
+	},
+	login: async ({ email, password }) => {
+		const user = User.findOne({ email: email });
+		if (!user) {
+			throw new Error("user does not exist!");
+		}
+		const isEqual = await bcrypt.compare(password, user.password);
+		if (!isEqual) {
+			throw new Error("Password is incorrect!");
+		}
+		const token = jwt.sign({ userId: user.id, email: user.email }, secret, {
+			expiresIn: "1h",
+		});
+		return { userId: user.id, token: token, tokenExpiration: 1 };
+	},
 };
--- server/graphql/schema/index.js
+++ server/graphql/schema/index.js
@@ -26,6 +26,13 @@ module.exports = buildSchema(
         email: String!
         password: String
         createdAppts: [Appt!]
+
+    }
+
+    type AuthData {
+        userId:ID!
+        token: String!
+        tokenExpiration: Int!
     }
 
     input ApptInput {
@@ -45,6 +52,7 @@ module.exports = buildSchema(
     type RootQuery{
         appt: [Appt!]!
         bookings: [Booking!]!
+        login(email: String!, password: String!): AuthData!
         
     }
 
