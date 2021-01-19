79870bcbfb6061e03c94ffb80d102fd1b5b01742
Jennifer Schlierman <jd9913@gmail.com>
2021-01-19 09:59:05 -0700

    change folder names

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
 
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
@@ -9,48 +9,47 @@ import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Success from "./pages/Success";
-import Nav from "./components/Nav";
+import Header from "./components/Header";
 import OrderHistory from "./pages/OrderHistory";
 
 // replaced {storeProvider } with following:
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 // Import the Redux store you created
-import store from './utils/store';
-
+import store from "./utils/store";
 
 const client = new ApolloClient({
-  request: (operation) => {
-    const token = localStorage.getItem('id_token')
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : ''
-      }
-    })
-  },
-  uri: '/graphql',
-})
+	request: (operation) => {
+		const token = localStorage.getItem("id_token");
+		operation.setContext({
+			headers: {
+				authorization: token ? `Bearer ${token}` : "",
+			},
+		});
+	},
+	uri: "/graphql",
+});
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div>
-          <Provider store={store}>
-            <Nav />
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/signup" component={Signup} />
-              <Route exact path="/orderHistory" component={OrderHistory} />
-              <Route exact path="/styles/:id" component={Detail} />
-              <Route exact path="/success" component={Success} />
-              <Route component={NoMatch} />
-            </Switch>
-          </Provider>
-        </div>
-      </Router>
-    </ApolloProvider>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<Router>
+				<div>
+					<Provider store={store}>
+						<Header />
+						<Switch>
+							<Route exact path='/' component={Home} />
+							<Route exact path='/login' component={Login} />
+							<Route exact path='/signup' component={Signup} />
+							<Route exact path='/orderHistory' component={OrderHistory} />
+							<Route exact path='/styles/:id' component={Detail} />
+							<Route exact path='/success' component={Success} />
+							<Route component={NoMatch} />
+						</Switch>
+					</Provider>
+				</div>
+			</Router>
+		</ApolloProvider>
+	);
 }
 
 export default App;
