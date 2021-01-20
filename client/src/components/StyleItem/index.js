5d6a3d6191eb7031d0b98a25442d27d1bf29ffb9
Jennifer Schlierman <jd9913@gmail.com>
2021-01-20 14:14:30 -0700

    adding cards to gallery

--- client/src/components/StyleItem/index.js
+++ client/src/components/StyleItem/index.js
@@ -1,68 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import { pluralize } from "../../utils/helpers"
-import { useDispatch, useSelector } from 'react-redux';
-import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { Card } from "react-bootstrap";
+
+import { useDispatch, useSelector } from "react-redux";
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 
 function StyleItem(item) {
-  const dispatch = useDispatch();
-  const state = useSelector(state => state);
+	const dispatch = useDispatch();
+	const state = useSelector((state) => state);
 
-  const {
-    image,
-    name,
-    _id,
-    price,
-    quantity
-  } = item;
+	const { image, name, _id, price, quantity } = item;
 
+	const { cart } = state;
 
-  const { cart } = state;
+	const addToCart = () => {
+		// find the cart item with the matching id
+		const itemInCart = cart.find((cartItem) => cartItem._id === _id);
 
-  const addToCart = () => {
-    // find the cart item with the matching id
-    const itemInCart = cart.find((cartItem) => cartItem._id === _id);
-  
-    // if there was a match, call UPDATE with a new purchase quantity
-    if (itemInCart) {
-      dispatch({
-        type: UPDATE_CART_QUANTITY,
-        _id: _id,
-        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
-      });
-      idbPromise('cart', 'put', {
-        ...itemInCart,
-        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
-      });
-    } else {
-      dispatch({
-        type: ADD_TO_CART,
-        style: { ...item, purchaseQuantity: 1 }
-      });
-      idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
-    }
-  };
+		// if there was a match, call UPDATE with a new purchase quantity
+		if (itemInCart) {
+			dispatch({
+				type: UPDATE_CART_QUANTITY,
+				_id: _id,
+				purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+			});
+			idbPromise("cart", "put", {
+				...itemInCart,
+				purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+			});
+		} else {
+			dispatch({
+				type: ADD_TO_CART,
+				style: { ...item, purchaseQuantity: 1 },
+			});
+			idbPromise("cart", "put", { ...item, purchaseQuantity: 1 });
+		}
+	};
 
-  return (
-    <div className='card border-secondary  mb-3'>
-      <Link to={`/styles/${_id}`}>
-        <img
-          className='style-image' 
-          alt={name}
-          src={`/images/${image}`}
-        />
-        <div className='card-header'>
-       {name}
-        </div>
-      </Link>
-      <div>
-        <div>{quantity} Style is avaliable</div>
-        <span>${price}</span>
-      </div>
-      <button onClick={addToCart}>Add to cart</button>
-    </div>
-  );
+	return (
+		<Card className='card border-secondary  mb-3'>
+			<Link to={`/styles/${_id}`}>
+				<img className='style-image' alt={name} src={`/images/${image}`} />
+				<div className='card-header'>{name}</div>
+			</Link>
+			<div>
+				<div>{quantity} Style is avaliable</div>
+				<span>${price}</span>
+			</div>
+			<button onClick={addToCart}>Add to cart</button>
+		</Card>
+	);
 }
 
 export default StyleItem;
