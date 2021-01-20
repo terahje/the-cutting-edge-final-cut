b5d152db04e7184da2fba46470badd8c84b880e0
Jennifer Schlierman <jd9913@gmail.com>
2021-01-20 11:46:13 -0700

    replaced all files to try and get it to work

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,107 +1,108 @@
 import React, { useEffect } from "react";
-import CartItem from "../CartItem";
-import Auth from "../../utils/auth";
+import CartItem from '../CartItem';
+import Auth from '../../utils/auth';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
-import { QUERY_CHECKOUT } from "../../utils/queries";
-import { loadStripe } from "@stripe/stripe-js";
-import { useLazyQuery } from "@apollo/react-hooks";
-import { useDispatch, useSelector } from "react-redux";
-import "./style.css";
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/react-hooks';
+import { useDispatch, useSelector } from 'react-redux';
+import './style.css';
 
-const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
-	const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-	const cart = useSelector((state) => state.cart);
-	const cartOpen = useSelector((state) => state.cartOpen);
+    const cart = useSelector(state => state.cart);
+    const cartOpen = useSelector(state => state.cartOpen);
+  
+  useEffect(() => {
+      if (!cart.length) {
+          (async () => {
+              const cart = await idbPromise('cart', 'get');
+              dispatch({ type: ADD_MULTIPLE_TO_CART, styles: [...cart] });
+          })()
+      }
+  }, [cart.length, dispatch]);
 
-	useEffect(() => {
-		if (!cart.length) {
-			(async () => {
-				const cart = await idbPromise("cart", "get");
-				dispatch({ type: ADD_MULTIPLE_TO_CART, styles: [...cart] });
-			})();
-		}
-	}, [cart.length, dispatch]);
+    const toggleCart = () => {
+    dispatch({ type: TOGGLE_CART });
+      
+    }
 
-	const toggleCart = () => {
-		dispatch({ type: TOGGLE_CART });
-	};
+    const calculateTotal = () => {
+        let sum = 0;
+        cart.forEach(item => {
+        sum += item.price * item.purchaseQuantity;
+        });
+        return sum.toFixed(2);
+    }
 
-	const calculateTotal = () => {
-		let sum = 0;
-		cart.forEach((item) => {
-			sum += item.price * item.purchaseQuantity;
-		});
-		return sum.toFixed(2);
-	};
+    const submitCheckout = () => {
+        const styleIds = [];
+      
+        cart.forEach((item) => {
+          for (let i = 0; i < item.purchaseQuantity; i++) {
+            styleIds.push(item._id);
+          }
+        });
 
-	const submitCheckout = () => {
-		const styleIds = [];
+        getCheckout({
+            variables: { styles: styleIds }
+          });
+      }
+      const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
-		cart.forEach((item) => {
-			for (let i = 0; i < item.purchaseQuantity; i++) {
-				styleIds.push(item._id);
-			}
-		});
+      useEffect(() => {
+        if (data) {
+          stripePromise.then((res) => {
+            res.redirectToCheckout({ sessionId: data.checkout.session });
+          });
+        }
+      }, [data]);
 
-		getCheckout({
-			variables: { styles: styleIds },
-		});
-	};
-	const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+  if (!cartOpen) {
+      return (
+        <div className="cart-closed" onClick={toggleCart}>
+          <span
+            role="img"
+            aria-label="trash">🧺</span>
+        </div>
+      );
+    }
 
-	useEffect(() => {
-		if (data) {
-			stripePromise.then((res) => {
-				res.redirectToCheckout({ sessionId: data.checkout.session });
-			});
-		}
-	}, [data]);
-
-	if (!cartOpen) {
-		return (
-			<div className='cart-closed' onClick={toggleCart}>
-				<span role='img' aria-label='barberpole'>
-					💈🛍️
-				</span>
-			</div>
-		);
-	}
-
-	return (
-		<div className='cart'>
-			<div className='close' onClick={toggleCart}>
-				[close]
-			</div>
-			<h2>Style Basket</h2>
-			{cart.length ? (
-				<div>
-					{cart.map((item) => (
-						<CartItem key={item._id} item={item} />
-					))}
-					<div className='flex-row space-between'>
-						<strong>Total: ${calculateTotal()}</strong>
-						{Auth.loggedIn() ? (
-							<button onClick={submitCheckout}>Checkout</button>
-						) : (
-							<span>(log in to check out)</span>
-						)}
-					</div>
-				</div>
-			) : (
-				<h4>
-					<span role='img' aria-label='shocked'>
-						💇
-					</span>
-					Your new hair style awaits you! Choose a style and we'll find you a
-					stylist!
-				</h4>
-			)}
-		</div>
-	);
+  return (
+    <div className="cart">
+        <div className="close" onClick={toggleCart}>[close]</div>
+            <h2>Style Basket</h2>
+            {cart.length ? (
+                <div>
+                {cart.map(item => (
+                    <CartItem key={item._id} item={item} />
+                ))}
+                <div className="flex-row space-between">
+                    <strong>Total: ${calculateTotal()}</strong>
+                    {
+                    Auth.loggedIn() ?
+                    <button onClick={submitCheckout}>
+                        Checkout
+                    </button>
+                        :
+                        <span>(log in to check out)</span>
+                    }
+                </div>
+                </div>
+            ) : (
+                <h4>
+                <span role="img" aria-label="shocked">
+                💇
+                </span>
+                Your new hair style awaits you! Choose a style and we'll find you a stylist!
+                </h4>
+            )}
+            </div>
+        );
 };
 
 export default Cart;
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,32 +2,44 @@ import React from "react";
 import Auth from "../../utils/auth";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
-// import { LinkContainer } from "react-router-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import logoImage from "../../assets/logo.png";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons'
+
+// // const cal = <FontAwesomeIcon icon= {faCalendarAlt} />
 
 function Header() {
 	function showNavigation() {
 		if (Auth.loggedIn()) {
 			return (
 				<header>
-					<Navbar className='navbar navbar-expand-lg navbar-dark bg-primary'>
-						<div className='navbar-collapse' id='navbarColor01'>
+					<Navbar
+						className='navbar navbar-expand-lg navbar-dark bg-primary'
+						collapseOnSelect>
+						<Container id='navbarColor01'>
+							<Navbar.Toggle aria-controls='basic-navbar-nav' />
 							<ul className='navbar-nav mr-auto'>
 								<li className='nav-item'>
-									<Nav.Link to='/orderHistory'>
-										<i className='far fa-calendar-alt'> </i> Appointments
-									</Nav.Link>
+									<LinkContainer to='/orderHistory'>
+										<Nav.Link className='nav-link' to='/orderHistory'>
+										{/* <h1>
+											<FontAwesomeIcon icon={farCalendarAlt} />
+										</h1> */}
+										Appointments
+										</Nav.Link>
+									</LinkContainer>
 								</li>
 								<li className='nav-item'>
-									<a
+									<Nav.Link
 										className='nav-link'
 										href='/'
 										onClick={() => Auth.logout()}>
 										Logout
-									</a>
+									</Nav.Link>
 								</li>
 							</ul>
-						</div>
+						</Container>
 					</Navbar>
 				</header>
 			);
@@ -38,10 +50,14 @@ function Header() {
 						<div className='navbar-collapse' id='navbarColor01'>
 							<ul className='navbar-nav mr-auto'>
 								<li className='nav-item'>
-									<Nav.Link to='/signup'>Signup</Nav.Link>
+									<Link className='nav-link' to='/signup'>
+										Signup
+									</Link>
 								</li>
 								<li className='nav-item'>
-									<Nav.Link to='/login'>Login</Nav.Link>
+									<Link className='nav-link' to='/login'>
+										Login
+									</Link>
 								</li>
 							</ul>
 						</div>
@@ -55,10 +71,10 @@ function Header() {
 		<header className='flex-row'>
 			<Navbar className='navbar navbar-expand-lg navbar-dark bg-primary'>
 				<img src={logoImage} className='logo-image' alt='logo' />
-				<Nav.Link to='/'>
+				<Link to='/'>
 					<span role='img' aria-label='shopping bag'></span>
 					<Navbar.Brand> The Cutting Edge</Navbar.Brand>
-				</Nav.Link>
+				</Link>
 			</Navbar>
 			<nav>{showNavigation()}</nav>
 		</header>
