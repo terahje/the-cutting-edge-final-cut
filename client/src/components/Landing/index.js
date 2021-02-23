99dba17d5fe62ec1a644da9cfb16ecd88393d489
aksmith5239 <aklobby@gmail.com>
2021-02-23 07:38:45 -0700

    landing image uploaded

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -1,11 +1,18 @@
 import React from "react";
+import landingImage from "../../assets/salon.jpg";
+var sectionLanding= {
+  backgroundImage: `url(${landingImage})`
+}
 
 function Landing() {
   return (
     <div
-      style={{ height: 560, clear: "both", paddingTop: 120, textAlign: "center" }}
+      className='landingImage' style={sectionLanding}
     >
+      <h1>Welcome to the Cutting Edge!</h1>
      
+  
+
     </div>
   );
 }
