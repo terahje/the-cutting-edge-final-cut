caead1bcb4c7ae867444242657cd82aa4689ffca
aksmith5239 <aklobby@gmail.com>
2021-02-24 08:50:14 -0700

    landing page updates added image and buttons

--- client/src/components/Landing/index.js
+++ client/src/components/Landing/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Card } from "react-bootstrap";
-
+import { Card, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import landingImage from "../../assets/salon.jpg";
 var sectionLanding= {
   backgroundImage: `url(${landingImage})`
@@ -8,13 +8,23 @@ var sectionLanding= {
 
 function Landing() {
   return (
-    <div
-      className='landingImage' style={sectionLanding}
-    >
+    <div>
       <Card className='landingCard'>
-        <Card.Body>
-          <h1 className='landingTitle text-warning'>Welcome to the Cutting Edge!</h1>
-        </Card.Body>
+        <Card.Img
+        variant="top"
+        src={landingImage} className='landingImage' alt='salon' 
+        />
+        <Card.ImgOverlay className='landingOverlay'>
+          <Card.Title ><h1 className='landingTitle text-primary'>Welcome to the Cutting Edge</h1>
+          <h2 className='text-dark '><em>Your new style is a click away!</em></h2>
+          <Link to='/signup'>
+          <Button className='btn btn-outline-warning btn-lg'>Sign Up</Button>
+          </Link>
+          <Link to='/login'>
+          <Button className='btn btn-outline-warning btn-lg'>Login</Button>
+          </Link>
+          </Card.Title>
+        </Card.ImgOverlay>
       </Card>
       
      
