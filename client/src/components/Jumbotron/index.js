b6fab92a50cb88828b15b4a8600c8b0c17df81bb
aksmith5239 <aklobby@gmail.com>
2021-02-27 11:17:50 -0700

    success page styling updated

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -2,13 +2,8 @@ import React from "react";
 
 function Jumbotron({ children }) {
 	return (
-		<div
-			style={{
-				height: 560,
-				clear: "both",
-				paddingTop: 120,
-				textAlign: "center",
-			}}>
+		<div>
+			
 			{children}
 		</div>
 	);
